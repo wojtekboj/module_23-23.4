d3a9bd0a4f78e9e5ad4b8e26d0f569dc159fb10f
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-09-05 16:39:51 +0200

    Add project files

---
+++ server/controllers/lane.controller.js
@@ -0,0 +1,61 @@
+import Lane from '../models/lane';
+import Note from '../models/note';
+import uuid from 'uuid';
+
+export function getSomething(req, res) {
+  return res.status(200).end();
+}
+
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  const newLane = new Lane(req.body);
+
+  newLane.notes = [];
+
+  newLane.id = uuid();
+  newLane.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
+}
+
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+}
+
+export function deleteLane(req, res) {
+  Note.deleteMany({ id: req.params.laneId }, (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+export function updateLane(req, res) {
+  Lane.update({ id: req.params.laneId }, { name: req.body.name }).exec((err, name) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ name });
+  });
+}
