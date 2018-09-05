09dd3b1b4848ceada03d2a7556eff683d048f190
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-09-05 17:25:30 +0200

    Modification project files

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -2,10 +2,6 @@ import Lane from '../models/lane';
 import Note from '../models/note';
 import uuid from 'uuid';
 
-export function getSomething(req, res) {
-  return res.status(200).end();
-}
-
 export function addLane(req, res) {
   if (!req.body.name) {
     res.status(403).end();
@@ -34,19 +30,17 @@ export function getLanes(req, res) {
 }
 
 export function deleteLane(req, res) {
-  Note.deleteMany({ id: req.params.laneId }, (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-
   Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
-    if (err) {
+    if(err){
       res.status(500).send(err);
     }
 
-    lane.remove(() => {
-      res.status(200).end();
+    const notes = lane.notes;
+    const notesIds = notes.map(note => note.id);
+    Note.remove({id: {$in: notesIds}}).exec(err => {
+      lane.remove(() => {
+        res.status(200).end();
+      });
     });
   });
 }
--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -30,12 +30,12 @@ export function addNote(req, res) {
 }
 
 export function deleteNote(req, res) {
-  Note.findOne({ id: req.params.noteId }).exec((err, lane) => {
+  Note.findOne({ id: req.params.noteId }).exec((err, note) => {
     if (err) {
       res.status(500).send(err);
     }
 
-    lane.remove(() => {
+    note.remove(() => {
       res.status(200).end();
     });
   });
